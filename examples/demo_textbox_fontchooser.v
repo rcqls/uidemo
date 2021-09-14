6c13d48f0de774866a05b0d7e9bc1495eb46f06b
R cqls <rdrouilh@gmail.com>
2021-09-14 05:00:07 +0200

    some fixes

--- examples/demo_textbox_fontchooser.v
+++ examples/demo_textbox_fontchooser.v
@@ -59,6 +59,6 @@ fn on_draw(c &ui.CanvasLayout, app &App) {
 	dtw.load_current_style()
 	c.draw_text(10, 10, app.text)
 	w, h := dtw.text_size(app.text)
-	c.draw_empty_rect(10, 11, w + 2, h + 2)
+	c.draw_empty_rect(10, 11, w + 2, h + 2, gx.black)
 	c.draw_styled_text(10 + w + 10, 10, 'size: ($w, $h)', 'default')
 }
